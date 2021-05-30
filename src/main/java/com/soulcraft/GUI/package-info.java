269ed8830a7aae89c8d14e6950676bdfc6328a58
Sommod <jm0350@yahoo.com>
2021-05-29 22:29:59 -0500

    Added package information

---
+++ src/main/java/com/soulcraft/GUI/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Contains the GUI menus
+ */
+/**
+ * Conains the classes that are used for initializing and
+ * displaying the GUI menus to the given person.
+ * 
+ * @author Sommod
+ * @version 1.0
+ *
+ */
+package com.soulcraft.GUI;
