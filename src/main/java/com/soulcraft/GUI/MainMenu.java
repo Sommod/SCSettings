98099763dfa9c2e8613f63144e87f21e47bf84f4
Armeriness <jm0350@yahoo.com>
2021-05-31 17:18:59 -0500

    Added Main Menu class

---
+++ src/main/java/com/soulcraft/GUI/MainMenu.java
@@ -0,0 +1,27 @@
+package com.soulcraft.GUI;
+
+import java.io.File;
+
+import org.bukkit.entity.Player;
+
+import com.soulcraft.Data.SCSettingsManager;
+
+/**
+ * Handles the main menu for the plugin.
+ *
+ * @author Armeriness
+ * @author Sommod
+ * @version 1.0
+ *
+ */
+public class MainMenu extends AbstractMenu {
+
+	public MainMenu(SCSettingsManager manager, String name) {
+		super(manager, name, new File(manager.getPlugin().getDataFolder(), "Data/Gui Data/Main Menu.yml"));
+	}
+
+	@Override
+	public void open(Player player) {
+	}
+
+}
