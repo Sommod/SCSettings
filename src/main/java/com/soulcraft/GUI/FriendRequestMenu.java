1303d4da8988838ff0cb1cf8e8f70e2f6913a462
Sommod <jm0350@yahoo.com>
2021-06-03 13:38:35 -0500

    Completed class

---
+++ src/main/java/com/soulcraft/GUI/FriendRequestMenu.java
@@ -0,0 +1,56 @@
+package com.soulcraft.GUI;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import com.soulcraft.Data.SCSettingsManager;
+import com.soulcraft.Player.PlayerData;
+
+/**
+ * Creates a new menu that is used for the players opening
+ * their list of people who have sent them a friend request
+ *
+ * @author Sommod
+ * @version 1.0
+ *
+ */
+public class FriendRequestMenu extends AbstractMenu {
+
+	public FriendRequestMenu(SCSettingsManager manager, String name) {
+		super(manager, name, new File(manager.getPlugin().getDataFolder(), "Data/Gui Data/Friend Request.yml"));
+	}
+
+	@Override
+	public void open(Player player) {
+		Inventory toOpen = getBaseInventory();
+		PlayerData pData = getManager().getPlayerManager().getPlayerData(player);
+		
+		for(int i = 10, k = 0; i < toOpen.getSize() - 9 && k < pData.getAllFriendRequests().size(); i++) {
+			if((i + 1) % 9 == 0) {
+				i++;
+				continue;
+			}
+			
+			ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+			SkullMeta meta = (SkullMeta) head.getItemMeta();
+			
+			meta.setOwningPlayer(pData.getAllFriendRequests().get(k));
+			meta.setDisplayName("§b" + pData.getAllFriendRequests().get(k).getName());
+			meta.setLore(Arrays.asList("§6Left Click: §aAccept", "§6Right Click: §cDecline"));
+			
+			head.setItemMeta(meta);
+			
+			toOpen.setItem(i, head);
+		}
+		
+		player.closeInventory();
+		player.openInventory(toOpen);
+	}
+
+}
