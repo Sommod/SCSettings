a7219784a274c2d0b7a8a71774173f6c59a13995
Sommod <jm0350@yahoo.com>
2021-06-03 16:57:48 -0500

    Added Package Information

---
+++ src/main/java/com/soulcraft/Commands/package-info.java
@@ -0,0 +1,12 @@
+/**
+ *  Command Handling
+ */
+/**
+ * Contains all the classes for each of the commands
+ * that are used for the plguin.
+ *
+ * @author Sommod
+ * @version 1.0
+ *
+ */
+package com.soulcraft.Commands;
