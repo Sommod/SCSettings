6bc9de26527215b40687a7d3695475a897b8d3bb
Sommod <jm0350@yahoo.com>
2021-06-05 18:52:21 -0500

    Added package information

---
+++ src/main/java/com/soulcraft/Items/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Item Handling
+ */
+/**
+ * Contains the classes that are used for handling items sent
+ * as gifts to friends as well as storage the items into the
+ * Admin Storage when the time of the item runs out.
+ *
+ * @author Sommod
+ * @version 1.0
+ *
+ */
+package com.soulcraft.Items;
