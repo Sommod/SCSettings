c502ce46d4d5ee1454b19a8ff8abccf4f0b76acb
Sommod <jm0350@yahoo.com>
2021-06-06 23:25:00 -0500

    Initialized opening of Admin storage GUI

--- src/main/java/com/soulcraft/GUI/AdminStorage.java
+++ src/main/java/com/soulcraft/GUI/AdminStorage.java
@@ -3,6 +3,7 @@ package com.soulcraft.GUI;
 import java.io.File;
 
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 import com.soulcraft.Data.SCSettingsManager;
 
@@ -18,12 +19,30 @@ import com.soulcraft.Data.SCSettingsManager;
  */
 public class AdminStorage extends AbstractMenu {
 
+	
+	//TODO: Create info for constructor
+	//TODO: Create a new file pointer to file location
 	public AdminStorage(SCSettingsManager manager, String name, File configFile) {
 		super(manager, name, configFile);
 	}
 
 	@Override
 	public void open(Player player) {
+		Inventory inv = getBaseInventory();
+		
+		for(int i = 10, k = 0; i < inv.getSize() && k < getManager().getItemManager().getAdminStorage().size(); i++) {
+			if((i + 1) % 9 == 0) {
+				i++;
+				continue;
+			}
+			
+			inv.setItem(i, getManager().getItemManager().getAdminStorage().get(k).getDisplayItem());
+		}
+		
+		if(isUsingFillOption() && !isBorder())
+			inv = getFilledInventory(inv);
+		
+		player.openInventory(inv);
 	}
 
 }
