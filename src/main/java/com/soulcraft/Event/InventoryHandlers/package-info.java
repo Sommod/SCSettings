980dc3209e7c52c3c76962dbcf5fb92bfc7128cb
Sommod <jm0350@yahoo.com>
2021-06-11 19:51:20 -0500

    Added information

---
+++ src/main/java/com/soulcraft/Event/InventoryHandlers/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * This contains the Class that handle each menu
+ */
+/**
+ * This contains the classes that handle and manipulate
+ * the menus and inventories that the player is currently
+ * using.
+ *
+ * @author Sommod
+ * @version 1.0
+ *
+ */
+package com.soulcraft.Event.InventoryHandlers;
---
+++ src/main/java/com/soulcraft/Event/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Event Handling
+ */
+/**
+ * Handles events that are used for the GUI systems
+
+ * @author Sommod
+ * @version 1.0
+ *
+ */
+package com.soulcraft.Event;
