232bdcc0803d1f7b71b98fb85a9956d5c30da295
Sommod <jm0350@yahoo.com>
2021-06-13 14:08:35 -0500

    Main Abstract Class for Event Handling

--- src/main/java/com/soulcraft/Event/AbstractHandler.java
+++ src/main/java/com/soulcraft/Event/AbstractHandler.java
@@ -23,15 +23,41 @@ public abstract class AbstractHandler {
 		this.event = event;
 	}
 	
+	/**
+	 * Gets the manager class of the plugin.
+	 * @return SCSettingsManager
+	 */
 	protected SCSettingsManager getManager() { return manager; }
 	
+	/**
+	 * Gets the ClickEvent that contains all
+	 * the stored data.
+	 * @return InventoryClickEVent
+	 */
 	public InventoryClickEvent getEvent() { return event; }
 	
+	/**
+	 * Gets the player who cliked in the inventory.
+	 * @return Player
+	 */
 	public Player getPlayer() { return (Player) event.getWhoClicked(); }
 	
+	/**
+	 * Gets the Top Inventory of the event.
+	 * @return Top Inventory
+	 */
 	public Inventory getTopInventory() { return event.getInventory(); }
 	
+	/**
+	 * Checks if the InvetnroryClickEvent was performed on the top
+	 * inventory
+	 * @return True - if clicked on the top inventory
+	 */
 	public boolean isTopInventory() { return event.getClickedInventory() == null ? false : event.getClickedInventory().equals(getTopInventory()); }
 	
+	/**
+	 * Checks if the clicked section of the inventory was done on the outside.
+	 * @return True - if clicked outside the inventories
+	 */
 	public boolean clickedOutside() { return event.getClickedInventory() == null; }
 }
