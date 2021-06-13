785a7ade8136f8eaa13de43709ab47f473c88f08
Sommod <jm0350@yahoo.com>
2021-06-13 17:39:21 -0500

    Completed GuiManager

--- src/main/java/com/soulcraft/GUI/GuiManager.java
+++ src/main/java/com/soulcraft/GUI/GuiManager.java
@@ -14,7 +14,7 @@ import com.soulcraft.Data.SCSettingsManager;
  */
 public class GuiManager {
 
-	private Map<String, AbstractMenu> menus;
+	private Map<Class<?>, AbstractMenu> menus;
 	private SCSettingsManager manager;
 	
 	public GuiManager(SCSettingsManager manager) {
@@ -23,9 +23,25 @@ public class GuiManager {
 	}
 	
 	private void init() {
-		menus = new HashMap<String, AbstractMenu>();
-		//TODO: Initialize Menus
-		//TODO: Put into Map
-		//TODO: Create Getter for Menu
+		menus = new HashMap<Class<?>, AbstractMenu>();
+		
+		menus.put(MainMenu.class, new MainMenu(manager));
+		menus.put(FriendsMenu.class, new FriendsMenu(manager));
+		menus.put(FriendRequestMenu.class, new FriendRequestMenu(manager));
+		menus.put(AddFriend.class, new AddFriend(manager));
+		menus.put(GiftMenu.class, new GiftMenu(manager));
+		menus.put(SettingsMenu.class, new SettingsMenu(manager));
+		menus.put(AdminStorage.class, new AdminStorage(manager));
 	}
+	
+	public MainMenu getMainMenu() { return getMenu(MainMenu.class); }
+	public FriendsMenu getFriendsMenu() { return getMenu(FriendsMenu.class); }
+	public FriendRequestMenu getFriendRequestMenu() { return getMenu(FriendRequestMenu.class); }
+	public AddFriend getAddFriend() { return (AddFriend) getMenu(AddFriend.class); }
+	public GiftMenu getGiftMenu() { return getMenu(GiftMenu.class); }
+	public SettingsMenu getSettingsMenu() { return getMenu(SettingsMenu.class); }
+	public AdminStorage getAdminStorage() { return getMenu(AdminStorage.class); }
+	
+	@SuppressWarnings("unchecked")
+	protected <T extends AbstractMenu> T getMenu(Class<T> clazz) { return (T) menus.get(clazz); }
 }
