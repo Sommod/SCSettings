0619e07c2803d6de07743e500553047ce3c66434
Sommod <jm0350@yahoo.com>
2021-06-18 09:03:56 -0500

    Completed Settings Menu Handler

---
+++ src/main/java/com/soulcraft/Event/InventoryHandlers/SettingsMenuHandler.java
@@ -0,0 +1,98 @@
+package com.soulcraft.Event.InventoryHandlers;
+
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+import com.soulcraft.Data.Perms;
+import com.soulcraft.Data.SCSettingsManager;
+import com.soulcraft.Event.AbstractHandler;
+import com.soulcraft.GUI.SettingsMenu;
+import com.soulcraft.Player.PlayerData;
+
+/**
+ * Handles the Settings Menu actions.
+ *
+ * @author Sommod
+ * @version 1.0
+ *
+ */
+public class SettingsMenuHandler extends AbstractHandler<SettingsMenu> {
+
+	public SettingsMenuHandler(InventoryClickEvent event, SCSettingsManager manager, SettingsMenu menu) {
+		super(event, manager, menu);
+	}
+	
+	@Override
+	public void execute() {
+		getEvent().setCancelled(true); // Defaulted to cancel every action
+		
+		if(isTopInventory()) {
+			if(getMenu().isButton(getEvent().getSlot())) {
+				switch (getMenu().getButton(getEvent().getSlot())) {
+				case MAIN_MENU:
+					getManager().getGuiManager().getMainMenu().open(getPlayer());
+					return;
+					
+				case FRIENDS_MENU:
+					getManager().getGuiManager().getFriendsMenu().open(getPlayer());
+					return;
+					
+				case FRIEND_REQUEST_MENU:
+					getManager().getGuiManager().getFriendRequestMenu().open(getPlayer());
+					return;
+					
+				case ADD_MENU:
+					getManager().getGuiManager().getAddFriend().open(getPlayer());
+					return;
+					
+				case ACCEPT_ALL:
+				case CONFIRM:
+				case NEXT_PAGE:
+				case PREVIOUS_PAGE:
+				case DECLINE_ALL:
+					return;
+					
+				case ERRROR:
+				default:
+					getPlayer().sendMessage("§Unknown Error, please contact the Administration. Error Code: SHM-21.");
+					break;
+				}
+			} else {
+				PlayerData data = getManager().getPlayerManager().getPlayerData(getPlayer());
+				
+				switch (getEvent().getSlot()) {
+				case 10:
+					if(getPlayer().hasPermission(Perms.DEATH_MESSAGES.toString()))
+							data.getChatSettings().setDeathMessages(!data.getChatSettings().isSeeingDeathMessages());
+					else
+						getPlayer().sendMessage(getManager().getFileManager().getConfig("config").getString("friend.no permission").replace('&', '§'));
+					return;
+					
+				case 11:
+					if(getPlayer().hasPermission(Perms.FRIEND_JOIN.toString()))
+						data.getChatSettings().setNotifyFriendJoin(!data.getChatSettings().isNotifiedFriendJoin());
+					else
+						getPlayer().sendMessage(getManager().getFileManager().getConfig("config").getString("friend.no permission").replace('&', '§'));
+					return;
+
+				case 12:
+					if(getPlayer().hasPermission(Perms.ALLOW_REQUESTS.toString()))
+						data.getChatSettings().setAllowFriendRequets(!data.getChatSettings().isAllowingFriendRequest());
+					else
+						getPlayer().sendMessage(getManager().getFileManager().getConfig("config").getString("friend.no permission").replace('&', '§'));
+					return;
+					
+				case 13:
+					if(getPlayer().hasPermission(Perms.IGNORE_CHAT.toString()))
+						data.getChatSettings().setIgnoreGlobalChat(!data.getChatSettings().isIgnoringGlobalChat());
+					else
+						getPlayer().sendMessage(getManager().getFileManager().getConfig("config").getString("friend.no permission").replace('&', '§'));
+					return;
+					
+				default:
+					return;
+				}
+			}
+		}
+	}
+
+}
