88125e3da05ba35559c181ca3aab6ebac42b9348
Sommod <jm0350@yahoo.com>
2021-06-19 16:39:49 -0500

    Fixed ERROR button name

--- src/main/java/com/soulcraft/GUI/AbstractMenu.java
+++ src/main/java/com/soulcraft/GUI/AbstractMenu.java
@@ -239,7 +239,7 @@ public abstract class AbstractMenu {
 	public enum Button {
 		MAIN_MENU, FRIENDS_MENU, ADD_MENU, SETTINGS_MENU, 
 		NEXT_PAGE, PREVIOUS_PAGE, FRIEND_REQUEST_MENU,
-		DECLINE_ALL, ACCEPT_ALL, CONFIRM, ERRROR;
+		DECLINE_ALL, ACCEPT_ALL, CONFIRM, ERROR;
 		
 		/**
 		 * Gets the Button Enumeruation based on the String value
@@ -254,7 +254,7 @@ public abstract class AbstractMenu {
 					return button;
 			}
 			
-			return ERRROR;
+			return ERROR;
 		}
 	}
 	
